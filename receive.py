87b4a2f7eb6630ec8b8b5051a7c1f889aea3ee0b
blakebeanz <78506279+blakebeanz@users.noreply.github.com>
2022-03-02 16:22:07 -0600

    Update receive.py
    update 3/2/22

--- receive.py
+++ receive.py
@@ -1,18 +1,12 @@
 from machine import Pin, UART
 from time import sleep
 
-
-uart = UART(0, 9600) # init with given baudrate
-
-user_input = ""
+uart0 = UART(0, 9600, bits = 8, parity=None, stop=1, tx=Pin(16), rx=Pin(17))
 
 while True:
-    if uart.any():
-        user_input = uart.readline()
+    if uart0.any() > 0:
+        data = uart0.read(10)
+        print(data.decode("UTF-8"))
         
-        #cleanup transmission that was recieved with the 'decode' attribute
-        print(user_input.decode("UTF-8"))
-        
-    else:
-        sleep(.1) #sleep to allow uart to read full transmission
+
         
